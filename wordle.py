e747f31ff79ed10e541cfc767cfcda1cebda235a
akhilesh-kafle <109803783+akhilesh-kafle@users.noreply.github.com>
2022-07-24 16:30:17 +0545

    fixed repeating problem

--- wordle.py
+++ wordle.py
@@ -4,6 +4,14 @@ from colorama import *
 words = ["seven","hello","crown","plant","money","earth","eight","other"]
 word_number = random.randint(0,len(words)-1)
 word = words[word_number]
+print("Rules of the game")
+print("Guess the WORDLE in six tries.")
+print("Each guess must be a valid five-letter word. Hit the enter button to submit.")
+print("After each guess, the color of the tiles will change to show how close your guess was to the word.")
+print("Green: The letter W is in the word and in the correct spot.")
+print("Yellow: The letter I is in the word but in the wrong spot.")
+print("Black:The letter U is not in the word in any spot.")
+print("")
 
 def start_game():
     play = True 
@@ -24,10 +32,11 @@ def start_game():
                         output = output + Back.BLACK + guess[i] + Back.RESET
                     else:
                         output = output + guess[i] + Back.RESET
-                    print(output)
+                print(output)
                 if guess == word:
                     print("Congrats! you did it")
             attempt = attempt + 1
+        print("You lost!")
         ask = input("Do you want to play again? y/n: ")
         if ask == "n":
             play = False
